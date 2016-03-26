7a824680b411319eb3544653302a979a34d77f25
Piotr Kowalski <piecioshka@gmail.com>
2016-03-26 20:04:31 +0100

    Split to hours & minutes

--- src/pebble-my-watch-face.c
+++ src/pebble-my-watch-face.c
@@ -1,20 +1,29 @@
 #include <pebble.h>
 
 static Window *s_main_window;
-static TextLayer *s_time_layer;
-static TextLayer *s_ip_layer;
+static TextLayer *s_hours_layer;
+static TextLayer *s_minutes_layer;
 
 static void update_time() {
     // Get a tm structure
     time_t temp = time(NULL);
     struct tm *tick_time = localtime(&temp);
 
+
     // Write the current hours and minutes into a buffer
-    static char s_buffer[8];
-    strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
+    static char s_hours_buffer[8];
+    strftime(s_hours_buffer, sizeof(s_hours_buffer), clock_is_24h_style() ? "%H" : "%I", tick_time);
 
     // Display this time on the TextLayer
-    text_layer_set_text(s_time_layer, s_buffer);
+    text_layer_set_text(s_hours_layer, s_hours_buffer);
+
+
+    // Write the current hours and minutes into a buffer
+    static char s_minutes_buffer[8];
+    strftime(s_minutes_buffer, sizeof(s_minutes_buffer), "%M", tick_time);
+
+    // Display this time on the TextLayer
+    text_layer_set_text(s_minutes_layer, s_minutes_buffer);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
@@ -28,36 +37,36 @@ static void main_window_load(Window *window) {
 
 
     // Create the TextLayer with specific bounds
-    s_time_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
+    s_hours_layer = text_layer_create(GRect(0, 0, bounds.size.w, 84));
 
     // Improve the layout to be more like a watchface
-    text_layer_set_background_color(s_time_layer, GColorClear);
-    text_layer_set_text_color(s_time_layer, GColorBlack);
-    text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_ROBOTO_BOLD_SUBSET_49));
-    text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+    text_layer_set_background_color(s_hours_layer, GColorClear);
+    text_layer_set_text_color(s_hours_layer, GColorBlack);
+    text_layer_set_font(s_hours_layer, fonts_get_system_font(FONT_KEY_ROBOTO_BOLD_SUBSET_49));
+    text_layer_set_text_alignment(s_hours_layer, GTextAlignmentCenter);
 
     // Add it as a child layer to the Window's root layer
-    layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+    layer_add_child(window_layer, text_layer_get_layer(s_hours_layer));
 
 
 
     // Create the TextLayer with specific bounds
-    s_ip_layer = text_layer_create(GRect(0, 0, bounds.size.w, 50));
+    s_minutes_layer = text_layer_create(GRect(0, 84, bounds.size.w, 84));
 
     // Improve the layout to be more like a watchface
-    text_layer_set_background_color(s_ip_layer, GColorClear);
-    text_layer_set_text_color(s_ip_layer, GColorBlack);
-    text_layer_set_font(s_ip_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
-    text_layer_set_text_alignment(s_ip_layer, GTextAlignmentCenter);
+    text_layer_set_background_color(s_minutes_layer, GColorClear);
+    text_layer_set_text_color(s_minutes_layer, GColorBlack);
+    text_layer_set_font(s_minutes_layer, fonts_get_system_font(FONT_KEY_ROBOTO_BOLD_SUBSET_49));
+    text_layer_set_text_alignment(s_minutes_layer, GTextAlignmentCenter);
 
     // Add it as a child layer to the Window's root layer
-    layer_add_child(window_layer, text_layer_get_layer(s_ip_layer));
+    layer_add_child(window_layer, text_layer_get_layer(s_minutes_layer));
 }
 
 static void main_window_unload(Window *window) {
     // Destroy TextLayer
-    text_layer_destroy(s_time_layer);
-    text_layer_destroy(s_ip_layer);
+    text_layer_destroy(s_hours_layer);
+    text_layer_destroy(s_minutes_layer);
 }
 
 // ----
